875b094a4fd58907b896c1b2440b3e88aee407ab
Vidottizzz <vidottizzz@gmail.com>
2021-12-15 11:23:02 -0300

    Funçao rotacionar imagem e show/hide da div

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
 
-function showhide(divElement) {
-    let div = document.getElementById(divElement);
+// // esconde e mostra o elemento div
+// function showHide() {
+//     let div = document.getElementById('div1');
 
-    if (div.style.display !== "none") {
-        div.style.display = "none";
+//     if (div.style.display !== "none") {
+//         div.style.display = "none";
+       
+//     }
+//     else {
+//         div.style.display = "block";
+//     }
+// }
+
+
+
+function toggle(ele) {
+    var div = document.getElementById("div1");
+    var = 
+    // for (let i = 0; i < )
+
+    
+    if (div.style.display == 'block') {
+        div.style.display = 'none';  
+        rotateImg(ele);  
     }
     else {
-        div.style.display = "block";
+        div.style.display = 'block';     
+        rotateImg(ele);
     }
 }
+
+
+
+
+
+
+// gira a imagem
+function rotateImg() {
+    let img = document.getElementById('img1');
+    
+
+    if(img.style.transform !== "rotate(180deg)")
+    {
+        img.style.transform = "rotate(180deg)";
+        
+    }
+    else {
+        img.style.transform = "rotate(0deg)";
+    }
+}
+
