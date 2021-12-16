0fcce440793ba78cb66cd21dec4029287ebd0114
Vidottizzz <vidottizzz@gmail.com>
2021-12-16 08:38:24 -0300

    show/hide com javascript, e rotate na seta

--- app.js
+++ app.js
@@ -1,52 +1,13 @@
+const accordionLi = document.querySelectorAll(".accordion");
 
-// // esconde e mostra o elemento div
-// function showHide() {
-//     let div = document.getElementById('div1');
+accordionLi.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("is-open");
 
-//     if (div.style.display !== "none") {
-//         div.style.display = "none";
-       
-//     }
-//     else {
-//         div.style.display = "block";
-//     }
-// }
-
-
-
-function toggle(ele) {
-    var div = document.getElementById("div1");
-    var = 
-    // for (let i = 0; i < )
-
+    let content = this.nextElementSibling;
+    console.log(content);
+    //ternario
+    accordion.classList.contains("is-open") ? content.classList.add("show") : content.classList.remove("show")
     
-    if (div.style.display == 'block') {
-        div.style.display = 'none';  
-        rotateImg(ele);  
-    }
-    else {
-        div.style.display = 'block';     
-        rotateImg(ele);
-    }
-}
-
-
-
-
-
-
-// gira a imagem
-function rotateImg() {
-    let img = document.getElementById('img1');
-    
-
-    if(img.style.transform !== "rotate(180deg)")
-    {
-        img.style.transform = "rotate(180deg)";
-        
-    }
-    else {
-        img.style.transform = "rotate(0deg)";
-    }
-}
-
+  };
+});
